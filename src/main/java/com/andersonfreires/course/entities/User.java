337099fa2bf43038389d1789b7c6483506cd1674
Anderson freires <andersonfreires1979@gmail.com>
2026-04-27 11:21:45 -0300

    H2 database, test profile, JPA

--- src/main/java/com/andersonfreires/course/entities/User.java
+++ src/main/java/com/andersonfreires/course/entities/User.java
@@ -4,9 +4,21 @@ import java.io.Serializable;
 import java.security.KeyStore.PrivateKeyEntry;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable   {
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idLong;
 	private String nameString;
 	private String emailString;
